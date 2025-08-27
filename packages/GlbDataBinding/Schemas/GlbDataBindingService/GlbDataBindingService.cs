d753165041271fad5bd39a82d0f5569bd3e033a4
Federico Buffa <fbuffa@solutionsmetrix.com>
2025-08-27 15:09:16 +0200

    changing in service class to be compatible with netcore in linux instances

--- packages/GlbDataBinding/Schemas/GlbDataBindingService/GlbDataBindingService.cs
+++ packages/GlbDataBinding/Schemas/GlbDataBindingService/GlbDataBindingService.cs
@@ -9,63 +9,65 @@ namespace Terrasoft.Configuration.GlbDataBindingService
 	using Terrasoft.Core.Factories;
 	using Terrasoft.Nui.ServiceModel.DataContract;
 	using Terrasoft.Web.Common;
+	using System.Web.SessionState;
 
 	#region Class: GlbDataBindingService
-
+	
 	[ServiceContract]
 	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
-	class GlbDataBindingService : BaseService
+	public class GlbDataBindingService : BaseService
 	{
-
 		#region Methods: Public
-
+		
 		[OperationContract]
 		[WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
-		public string GetBindDataPackages(Request request) {
+		public string GetBindDataPackages(Request request) 
+		{
 			var helper = ClassFactory.Get<GlbDataBindingHelper>(
 				new ConstructorArgument("userConnection", UserConnection)
 			);
 			var result = helper.GetPackages(request.SchemaName);
-			return ServiceStack.Text.JsonSerializer.SerializeToString(result);
+			return Newtonsoft.Json.JsonConvert.SerializeObject(result);
 		}
-
+		
 		[OperationContract]
 		[WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
-		public Response RestoreColumnSetup(Request request) {
+		public Response RestoreColumnSetup(Request request) 
+		{
 			var helper = ClassFactory.Get<GlbDataBindingHelper>(
 				new ConstructorArgument("userConnection", UserConnection)
 			);
 			var result = helper.ClearColumnsSetup(request.ProfileKey);
 			return result;
 		}
-
+		
 		#endregion
 	}
-
+	
 	#endregion
 
 	#region Class: Response
-
+	
 	[DataContract]
-	public class Response: SelectQueryResponse
+	public class Response : SelectQueryResponse
 	{		
 		[DataMember(Name = "message")]
-		public string Message {get; set;}
+		public string Message { get; set; }
 	}
-
+	
 	#endregion
 
 	#region Class: Request
-
+	
 	[DataContract]
 	public class Request
 	{
 		[DataMember(Name = "schemaName")]
-		public string SchemaName {get; set;}
-
+		public string SchemaName { get; set; }
+		
 		[DataMember(Name = "profileKey")]
-		public string ProfileKey {get; set;}
+		public string ProfileKey { get; set; }
 	}
-
+	
 	#endregion
 }
